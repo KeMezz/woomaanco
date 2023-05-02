d13e0f99036ebd4053dcbe033e6f57173eb975ec
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-02 16:15:00 +0900

    비활성과 활성 아이콘을 구분

--- src/components/footer-icon.tsx
+++ src/components/footer-icon.tsx
@@ -7,28 +7,42 @@ interface FooterIconProps {
   id: "home" | "trending" | "dm" | "profile";
 }
 
+interface FooterItem {
+  title: string;
+  link: string;
+  inactive: JSX.Element;
+  active: JSX.Element;
+}
+
+interface FooterItems {
+  home: FooterItem;
+  trending: FooterItem;
+  dm: FooterItem;
+  profile: FooterItem;
+}
+
 const FooterIcon: NextPage<FooterIconProps> = ({ id }) => {
   const router = useRouter();
-  const isMatch = router.pathname.split("/")[1] === icons[id].link;
+  const isMatch = router.pathname.split("/")[1] === footerItems[id].link;
   return (
     <Link
-      href={`/${icons[id].link}`}
+      href={`/${footerItems[id].link}`}
       className={cls(
         "grid grid-rows-2 place-items-center gap-1 py-4 hover:bg-zinc-100",
-        isMatch ? "text-indigo-400" : "text-black"
+        isMatch ? "text-rose-500" : "text-black"
       )}
     >
-      {icons[id].svg}
-      <span className="text-sm">{icons[id].title}</span>
+      {isMatch ? footerItems[id].active : footerItems[id].inactive}
+      <span className="text-sm">{footerItems[id].title}</span>
     </Link>
   );
 };
 
-const icons = {
+const footerItems: FooterItems = {
   home: {
     title: "홈",
     link: "",
-    svg: (
+    inactive: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -44,11 +58,22 @@ const icons = {
         />
       </svg>
     ),
+    active: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-6 h-6"
+      >
+        <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
+        <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
+      </svg>
+    ),
   },
   trending: {
-    title: "인기",
+    title: "급상승",
     link: "trending",
-    svg: (
+    inactive: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -69,11 +94,25 @@ const icons = {
         />
       </svg>
     ),
+    active: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          fillRule="evenodd"
+          d="M12.963 2.286a.75.75 0 00-1.071-.136 9.742 9.742 0 00-3.539 6.177A7.547 7.547 0 016.648 6.61a.75.75 0 00-1.152-.082A9 9 0 1015.68 4.534a7.46 7.46 0 01-2.717-2.248zM15.75 14.25a3.75 3.75 0 11-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 011.925-3.545 3.75 3.75 0 013.255 3.717z"
+          clipRule="evenodd"
+        />
+      </svg>
+    ),
   },
   dm: {
     title: "DM",
     link: "dm",
-    svg: (
+    inactive: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -89,11 +128,22 @@ const icons = {
         />
       </svg>
     ),
+    active: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-6 h-6"
+      >
+        <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
+        <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
+      </svg>
+    ),
   },
   profile: {
     title: "프로필",
     link: "profile",
-    svg: (
+    inactive: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -106,6 +156,20 @@ const icons = {
           strokeLinecap="round"
           strokeLinejoin="round"
           d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+        />
+      </svg>
+    ),
+    active: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          fillRule="evenodd"
+          d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+          clipRule="evenodd"
         />
       </svg>
     ),
