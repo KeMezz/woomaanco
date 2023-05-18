aed8607efa3449e7c8bedeb15a7aea76c5eff3f3
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-18 13:56:46 +0900

    디자인 초안 작성

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,7 +3,12 @@ import Layout from "@components/layout";
 const Profile = () => {
   return (
     <Layout title="프로필">
-      <p>Profile!</p>
+      <div className="flex flex-col justify-center items-center px-4 py-8 bg-white m-4 rounded-lg shadow-sm">
+        <div className="w-32 h-32 bg-gray-200 rounded-full" />
+        <div className="flex flex-col justify-center items-center pt-4">
+          <h3 className="font-semibold text-lg">정형진</h3>
+        </div>
+      </div>
     </Layout>
   );
 };
