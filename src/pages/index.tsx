84f03ca7d1d8c1184768b19d1d41d105332696fb
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-05-19 13:49:34 +0900

    Create fib.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import FIB from "@components/fib";
 import Layout from "@components/layout";
 import Link from "next/link";
 
@@ -31,22 +32,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <button className="w-14 h-14 bg-rose-400 rounded-full flex justify-center items-center text-white fixed right-8 bottom-28 shadow-lg">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="w-8 h-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M12 4.5v15m7.5-7.5h-15"
-          />
-        </svg>
-      </button>
+      <FIB />
     </Layout>
   );
 }
